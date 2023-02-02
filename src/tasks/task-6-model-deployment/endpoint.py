809034a2607e5a444acddd5fc3158ce2f2a00ce3
Hyacinth Ampadu <ampaduh@gmail.com>
2023-02-02 20:01:12 +0000

    fixing some issues in task 6 regarding authentication

--- src/tasks/task-6-model-deployment/endpoint.py
+++ src/tasks/task-6-model-deployment/endpoint.py
@@ -1,5 +1,9 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-auth = ('ampaduh@gmail.com', 'pass1234')
+load_dotenv()
+
+auth = (os.environ.get("AUTHENTICATE_USERNAME"), os.environ.get("AUTHENTICATE_PASSWORD"))
 login_response = requests.get('http://127.0.0.1:5000/user/login', auth =auth)
 print(login_response.text)
